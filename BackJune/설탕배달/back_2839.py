c41308992b54145bdad9b5b01b65a9337a9b8caf
wmf34a <wmf34a@naver.com>
2020-09-15 23:05:51 +0900

    백준-설탕배달 풀이

---
+++ BackJune/설탕배달/back_2839.py
@@ -0,0 +1,15 @@
+sugar_kg = int(input())
+total_cnt = 0;
+
+while True:
+    if sugar_kg % 5 == 0:
+        total_cnt += sugar_kg//5
+        break
+    elif sugar_kg <= 0:
+        total_cnt = -1
+        break
+    sugar_kg -= 3
+    total_cnt +=1
+
+print(total_cnt)
+
