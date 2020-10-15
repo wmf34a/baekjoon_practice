a9bab3bf4ed479acef3b074e94522cbdb229a27f
Yeonseung Choi <wmf34a@naver.com>
2020-10-15 18:18:41 +0900

    백준 단계별 풀이 2577

--- BackJune/step_by_step/1_dimension_array/back_2577.py
+++ BackJune/step_by_step/1_dimension_array/back_2577.py
@@ -2,9 +2,12 @@
 import sys
 input = sys.stdin.readline
 
-arr = []
-for i in range(3):
-    n = int(input())
-    arr.append(n)
+A = int(input())
+B = int(input())
+C = int(input())
 
-print(arr)
+res = list(str(A*B*C))
+
+for i in range(0,10):
+    print(res.count(str(i)))
+
