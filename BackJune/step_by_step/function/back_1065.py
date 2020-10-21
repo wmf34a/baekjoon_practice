9cd3277b97f53b26744610c29e07886f8ed4207e
Yeonseung Choi <wmf34a@naver.com>
2020-10-21 16:08:31 +0900

    백준 단게별 풀이 셀프넘버 4673

---
+++ BackJune/step_by_step/function/back_1065.py
@@ -0,0 +1,8 @@
+#한수
+def main():
+    res=0
+    for i in range(1,11):
+        print(i)
+        res +=i
+    #print(res)
+main()
--- BackJune/step_by_step/function/back_4673.py
+++ BackJune/step_by_step/function/back_4673.py
@@ -1,1 +1,15 @@
 #셀프 넘버
+def main():
+    num_list = list(range(1,10001))
+    del_list = []
+    for num in num_list:
+        for n in str(num):
+            num += int(n)
+        if num <= 10000:
+            del_list.append(num)
+        
+    for del_num in set(del_list):
+        num_list.remove(del_num)
+    for res in num_list:
+        print(res)
+main()
