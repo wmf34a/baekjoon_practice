75e6b2bb712271dc6ff6d1a7932a010dc37304ff
Yeonseung Choi <wmf34a@naver.com>
2020-10-22 13:42:34 +0900

    백준 단계별 풀이 한수 1065

--- BackJune/step_by_step/function/back_1065.py
+++ BackJune/step_by_step/function/back_1065.py
@@ -1,8 +1,13 @@
 #한수
 def main():
-    res=0
-    for i in range(1,11):
-        print(i)
-        res +=i
-    #print(res)
+    N = int(input())
+    cnt = 0
+    for i in range(1, N+1):
+        if i <=99:
+            cnt +=1
+        else:
+            nums = list(map(int, str(i)))
+            if nums[0] - nums[1] == nums[1] - nums[2]:
+                cnt +=1
+    print(cnt)
 main()
